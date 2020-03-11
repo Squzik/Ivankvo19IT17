e366d955d59574d37e94bf8e24b7961359420838
Squzik <Lucaspw@yandex.ru>
2020-03-11 17:34:32 +0300

    1.Готово Релиз

--- cal/cal/Form1.cs
+++ cal/cal/Form1.cs
@@ -22,21 +22,12 @@ namespace cal
         {
             InitializeComponent();
         }
-
-
-        
-        
-
-
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
+            
         private void button2_Click_1(object sender, EventArgs e)
         {
 
-            label2.Text = $"{1}";
+            label2.Text = "-";
+            c = 1;
 
 
         }
@@ -44,72 +35,56 @@ namespace cal
         private void button1_Click(object sender, EventArgs e)
         {
 
-            label2.Text = $"{0}";
+            label2.Text = "+";
+            c = 2;
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            label2.Text = $"{2}";
+            label2.Text = "*";
+            c = 3;
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
 
-            label2.Text = $"{3}";
+            label2.Text = "/";
+            c = 4;
 
         }
 
-        private void label1_Click(object sender, EventArgs e)
-        {
 
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
 
         private void button6_Click(object sender, EventArgs e)
         {
             textBox1.Clear();
             textBox2.Clear();
-            textBox3.Clear();
-            label1.Text="0"; 
+            label1.Text = "0";
+            label2.Text = "0";
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox3_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label2_Click(object sender, EventArgs e)
-        {
-            c = (Convert.ToInt32(label2.Text));
-        }
 
         private void button5_Click(object sender, EventArgs e)
         {
-            if (c == 0)
-            {
-                label1.Text = (Convert.ToInt32(textBox1.Text) + Convert.ToInt32(textBox2.Text)).ToString();
-            }
+           
             if (c == 1)
             {
                 label1.Text = (Convert.ToInt32(textBox1.Text) - Convert.ToInt32(textBox2.Text)).ToString();
             }
             if (c == 2)
             {
-                label1.Text = (Convert.ToInt32(textBox1.Text) * Convert.ToInt32(textBox2.Text)).ToString();
+                label1.Text = (Convert.ToInt32(textBox1.Text) + Convert.ToInt32(textBox2.Text)).ToString();
             }
             if (c == 3)
+            {
+                label1.Text = (Convert.ToInt32(textBox1.Text) * Convert.ToInt32(textBox2.Text)).ToString();
+            }
+            if (c == 4)
             {
                 label1.Text = (Convert.ToInt32(textBox1.Text) / Convert.ToInt32(textBox2.Text)).ToString();
             }
         }
+
+       
     }
 }
