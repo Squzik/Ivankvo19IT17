278eb923b37b63904c3b7c14a6f2004ecaa2a69c
Squzik <Lucaspw@yandex.ru>
2020-03-30 12:18:06 +0300

    test

--- cal/cal/Form1.cs
+++ cal/cal/Form1.cs
@@ -47,7 +47,7 @@ namespace cal
                 label2.Text = $"{a}-";
                
         }
-
+        //l
         private void plus(object sender, EventArgs e)//Кнопка +
         {
             obrabotka(textBox1.Text);
